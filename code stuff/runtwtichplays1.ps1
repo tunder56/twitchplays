9fe381e831234c1c527d0935f7a0d7d8f6b556e3
mtnrbqamp <65264071+tunder56@users.noreply.github.com>
2021-08-10 20:31:36 +1000

    Update runtwtichplays1.ps1

--- code stuff/runtwtichplays1.ps1
+++ code stuff/runtwtichplays1.ps1
@@ -1,5 +1,12 @@
-Set-Location C:\twitchplays
-conda activate twitchplays27
+Set-Location C:\code\twitchplayer
+conda activate twitch27
 Set-Location $home
-& /../miniconda3/envs/twitchplays27/python.exe C:/twitchplays/slectgame.py
+
+try {
+    & /../miniconda3/envs/twitch27/python.exe C:/code/twitchplayer/slectgame.py
+    C:\ProgramData\Miniconda3\envs\twitchplays27\python.exe  c:/twitchplays/slectgame.py
+}
+catch {
+    Write-Host "Error occured check your file paths and change me" -BackgroundColor Darkred
+}
 
