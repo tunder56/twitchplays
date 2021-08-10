8bd5a1b4f9dd97133b1052d1ba4c4acfecc33906
mtnrbqamp <65264071+tunder56@users.noreply.github.com>
2021-08-10 19:44:17 +1000

    Update runtwtichplays1.ps1

--- code stuff/runtwtichplays1.ps1
+++ code stuff/runtwtichplays1.ps1
@@ -1,5 +1,5 @@
 Set-Location C:\code\twitchplayer
-conda activate twitch27
+conda activate twitchplays27
 Set-Location $home
-& /../miniconda3/envs/twitch27/python.exe C:/twitchplays/slectgame.py
+& /../miniconda3/envs/twitchplays27/python.exe C:/twitchplays/slectgame.py
 
